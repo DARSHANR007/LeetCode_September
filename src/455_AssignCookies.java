5ddf025b655a9cbd59baf21df520c0512b1294a7
DARSHANR007 <darshanravichandran07@gmail.com>
2024-02-04 22:50:52 +0530

    Leetcode

---
+++ src/455_AssignCookies.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution455 {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int count = 0;
+        int i = 0;
+        int j = 0;
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                count++;
+                i++;
+            }
+            j++;
+        }
+
+        return count;
+    }
+}
